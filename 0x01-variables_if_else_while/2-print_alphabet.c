1b857ad2d7c9495b22416de34be9e0960cf6391b
Abdibaye <abdibaye0902@gmail.com>
2023-02-19 11:32:54 -0800

    lst digit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,30 +1,18 @@
 #include <stdio.h>
-
-/**
- * main - Writes 00 - 99
- * @void: Empty parameter list for main.
- *
- * Description: Writes number 00-99
- *
- * Return: 0 for success
+#include <string.h>
+/*
+ * main - Prints lowercase alphabets
+ * Return: Always 0 (success)
 */
+
 int main(void)
 {
-	int i, j;
+        char ch = 'a';
 
-	for (i = 48; i <= 57; i++)
-	{
-		for (j = 48; j <= 57; j++)
-		{
-			putchar(i);
-			putchar(j);
-			if (i + j < 114)
-			{
-				putchar(44);
-				putchar(32);
-			}
-		}
-	}
-	putchar(10);
-	return (0);
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+                putchar(ch);
+        }
+        putchar('\n');
+        return (0);
 }
