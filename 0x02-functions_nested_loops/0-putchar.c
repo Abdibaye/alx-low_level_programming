16cdb378967685bc199d6d5233883badc7160b14
Abdibaye <abdibaye0902@gmail.com>
2023-02-26 07:31:24 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,9 @@
  * main - Print _putchar
  * Return: Always 0
  */
+
 int main(void)
+
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,4 +18,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
+
 }
